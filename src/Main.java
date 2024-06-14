0c447be3edb7122f80d8e059513557415ef7631c
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-06-14 23:26:29 +0400

    O(n)

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,41 @@ public class Main {
         log(numbers);
     }
 
-    // O(1)
     public static void log(int[] numbers) {
+        //---------O(1)---------------
         // 0(2) // simplified O(1)
         System.out.println(numbers[0]); // O(1)
         System.out.println(numbers[0]); // O(1)
+
+        //---------O(n)---------------
+        // runtime complexity 0(n)
+        for (int i = 0; i < numbers.length; i++)
+            System.out.println(numbers[i]);
+
+        // with foreach
+        // runtime complexity 0(n)
+        for (int number : numbers)
+            System.out.println(number);
+
+        // runtime complexity 0(1+n+1)/O(2+n) // simplified O(n)
+        System.out.println(); // O(1)
+        for (int number : numbers) // O(n)
+            System.out.println(number);
+        System.out.println(); // O(1)
+
+        // runtime complexity O(n+n) // simplified O(n)
+        for (int number : numbers) // O(n)
+            System.out.println(number);
+        for (int number : numbers) // O(n)
+            System.out.println(number);
+    }
+
+    //---------O(n)---------------
+    public static void log(int[] numbers, String[] names) {
+        // runtime complexity O(n+m) // simplified O(n)
+        for (int number : numbers) // O(n)
+            System.out.println(number);
+        for (String name : names) // O(m)
+            System.out.println(name);
     }
 }
