a0d8c3cdd75b0e5af947014f2a336bd50b7bddac
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-07-30 23:07:55 +0400

    Hash Tables in Java.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Queue;
 //import java.util.PriorityQueue;
 //import java.util.Queue;
@@ -57,41 +59,41 @@ public class Main {
         list.toArray();
 
         // ************* Linked Lists ****************
-        //linkedList();
+        // linkedList();
 
         // ********** Building a Linked List **********
-//        var linkedList = new LinkedList();
-//        linkedList.addLast(10);
-//        linkedList.addLast(20);
-//        linkedList.addLast(30);
-//        linkedList.addLast(40);
-//        linkedList.addLast(50);
-//        linkedList.addLast(60);
-//        linkedList.addFirst(5);
-//        System.out.println(linkedList.indexOf(10)); // 1
-//        System.out.println(linkedList.contains(10)); // true
-//        linkedList.removeFirst();
-//        linkedList.removeLast();
-//        System.out.println(linkedList.size()); // 2
-//        linkedList.reverse();
-//        System.out.println(linkedList.getKthFromTheEnd(2)); // 20
-//        var arr = linkedList.toArray();
-//        System.out.println(Arrays.toString(arr));
-//       linkedList.printMiddle(); // 30, 40
-//        var listWithLoop = LinkedList.createWithLoop();
-//        System.out.println(listWithLoop.hasLoop());
+        // var linkedList = new LinkedList();
+        // linkedList.addLast(10);
+        // linkedList.addLast(20);
+        // linkedList.addLast(30);
+        // linkedList.addLast(40);
+        // linkedList.addLast(50);
+        // linkedList.addLast(60);
+        // linkedList.addFirst(5);
+        // System.out.println(linkedList.indexOf(10)); // 1
+        // System.out.println(linkedList.contains(10)); // true
+        // linkedList.removeFirst();
+        // linkedList.removeLast();
+        // System.out.println(linkedList.size()); // 2
+        // linkedList.reverse();
+        // System.out.println(linkedList.getKthFromTheEnd(2)); // 20
+        // var arr = linkedList.toArray();
+        // System.out.println(Arrays.toString(arr));
+        // linkedList.printMiddle(); // 30, 40
+        // var listWithLoop = LinkedList.createWithLoop();
+        // System.out.println(listWithLoop.hasLoop());
 
         // *************** Stacks *****************
-//        Stack<Integer> stack = new Stack<>();
-//        stack.push(10);
-//        stack.push(20);
-//        stack.push(30);
-//        System.out.println(stack); // [10, 20, 30]
-//        var top = stack.pop();
-//        System.out.println(top); // 30
-//        System.out.println(stack); // [10, 20]
-//        top = stack.peek();
-//        System.out.println(top); // 20
+        // Stack<Integer> stack = new Stack<>();
+        // stack.push(10);
+        // stack.push(20);
+        // stack.push(30);
+        // System.out.println(stack); // [10, 20, 30]
+        // var top = stack.pop();
+        // System.out.println(top); // 30
+        // System.out.println(stack); // [10, 20]
+        // top = stack.peek();
+        // System.out.println(top); // 20
 
         String str = "abcd";
         StringReverser reverser = new StringReverser();
@@ -140,8 +142,8 @@ public class Main {
         System.out.println(queue); // [20, 30, 40]
 
         // Reversing a Queue
-//        Queue.reverse(queue);
-//        System.out.println(queue);// [40, 30, 20]
+        // Queue.reverse(queue);
+        // System.out.println(queue);// [40, 30, 20]
 
         // Building a Queue Using an Array
         ArrayQueue arrayQueue = new ArrayQueue(5);
@@ -177,14 +179,14 @@ public class Main {
         System.out.println(first); // 10
 
         // Priority Queues
-//        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-//        priorityQueue.add(5);
-//        priorityQueue.add(1);
-//        priorityQueue.add(3);
-//        priorityQueue.add(2);
-//        while (!priorityQueue.isEmpty()) {
-//            System.out.println(priorityQueue.remove()); // 1 2 3 5
-//        }
+        // PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        // priorityQueue.add(5);
+        // priorityQueue.add(1);
+        // priorityQueue.add(3);
+        // priorityQueue.add(2);
+        // while (!priorityQueue.isEmpty()) {
+        // System.out.println(priorityQueue.remove()); // 1 2 3 5
+        // }
 
         // *********** Building a Priority Queue ************
         PriorityQueue prQueue = new PriorityQueue();
@@ -198,7 +200,7 @@ public class Main {
         while (!prQueue.isEmpty())
             System.out.println(prQueue.remove()); // 6 5 4 3 1
 
-        // ********** Exercise -  Queue Reverser ***************
+        // ********** Exercise - Queue Reverser ***************
         java.util.Queue<Integer> que = new ArrayDeque<>();
         que.add(10);
         que.add(20);
@@ -235,15 +237,47 @@ public class Main {
         System.out.println(pop); // 60
         System.out.println(stackWithTwoQueues); // [10, 20, 30, 40, 50]
 
+        // *********** Hash Tables ************************
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Mosh");
+        map.put(2, "John");
+        map.put(3, "Mary");
+        System.out.println(map); // {1=Mosh, 2=John, 3=Mary}
+        map.put(3, "Marianne");
+        System.out.println(map); // {1=Mosh, 2=John, 3=Marianne}
+        map.put(4, null);
+        System.out.println(map); // {1=Mosh, 2=John, 3=Marianne, 4=null}
+        map.put(null, null);
+        System.out.println(map); // {null=null, 1=Mosh, 2=John, 3=Marianne, 4=null}
+        map.remove(null);
+        System.out.println(map); // {1=Mosh, 2=John, 3=Marianne, 4=null}
+        var value = map.get(3);
+        System.out.println(value); // Marianne
+        map.containsKey(3); // O(1)
+        map.containsValue("Mosh"); // O(n)
+
+        for (var item : map.keySet())
+            System.out.println(item);
+        // 1
+        // 2
+        // 3
+        // 4
+
+        for (var item : map.entrySet())
+            System.out.println(item);
+        // 1=Mosh
+        // 2=John
+        // 3=Marianne
+        // 4=null
     }
 
     public static void log(int[] numbers) {
-        //---------O(1)---------------
+        // ---------O(1)---------------
         // 0(2) // simplified O(1)
         System.out.println(numbers[0]); // O(1)
         System.out.println(numbers[0]); // O(1)
 
-        //---------O(n)---------------
+        // ---------O(n)---------------
         // runtime complexity 0(n)
         for (int i = 0; i < numbers.length; i++)
             System.out.println(numbers[i]);
@@ -265,8 +299,7 @@ public class Main {
         for (int number : numbers) // O(n)
             System.out.println(number);
 
-
-        //---------O(n^2)---------------
+        // ---------O(n^2)---------------
         // runtime complexity O(n*n) or O(n^2)
         for (int first : numbers) // O(n)
             for (int second : numbers) // O(n)
@@ -286,7 +319,7 @@ public class Main {
                     System.out.println(first + ", " + second + ", " + third);
     }
 
-    //---------O(n)---------------
+    // ---------O(n)---------------
     public static void log(int[] numbers, String[] names) {
         // runtime complexity O(n+m) // simplified O(n)
         for (int number : numbers) // O(n)
@@ -295,18 +328,17 @@ public class Main {
             System.out.println(name);
     }
 
-
-    //---------Space Complexity---------------
+    // ---------Space Complexity---------------
     public void greet(String[] names) {
         // O(n) space
         String[] copy = new String[names.length];
 
         // O(1) space
-        for (int i=0; i<names.length; i++)
+        for (int i = 0; i < names.length; i++)
             System.out.println("Hi" + names[i]);
     }
 
-    //-----------Working with Arrays--------------
+    // -----------Working with Arrays--------------
     public static void array() {
         int[] numbers = new int[3];
         System.out.println(numbers); // [I@60addb54
@@ -320,28 +352,28 @@ public class Main {
         System.out.println(Arrays.toString(numbers)); // [10, 20, 30]
 
         // or
-        int[] nums = {10, 20, 30};
+        int[] nums = { 10, 20, 30 };
         System.out.println(Arrays.toString(nums)); // [10, 20, 30]
 
         System.out.println(nums.length); // 3
     }
 
-    //-------------Working with Linked Lists-----------------
-//    public static void linkedList() {
-//        LinkedList list = new LinkedList();
-//        list.add(10);
-//        list.add(20);
-//        list.add(30);
-//        System.out.println(list);
-//        list.addFirst(5);
-//        System.out.println(list); // [5, 10, 20, 30]
-//        System.out.println(list.contains(10)); // true
-//        System.out.println(list.indexOf(10)); // 1
-//        System.out.println(list.size()); // 4
-//        var array = list.toArray();
-//        System.out.println(Arrays.toString(array)); // [5, 10, 20, 30]
-//        list.removeLast();
-//        list.remove(1);
-//        list.removeFirst();
-//    }
+    // -------------Working with Linked Lists-----------------
+    // public static void linkedList() {
+    // LinkedList list = new LinkedList();
+    // list.add(10);
+    // list.add(20);
+    // list.add(30);
+    // System.out.println(list);
+    // list.addFirst(5);
+    // System.out.println(list); // [5, 10, 20, 30]
+    // System.out.println(list.contains(10)); // true
+    // System.out.println(list.indexOf(10)); // 1
+    // System.out.println(list.size()); // 4
+    // var array = list.toArray();
+    // System.out.println(Arrays.toString(array)); // [5, 10, 20, 30]
+    // list.removeLast();
+    // list.remove(1);
+    // list.removeFirst();
+    // }
 }
