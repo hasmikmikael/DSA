73242e53b8cf02aa9594677cecdd31df86dbcc90
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-08-12 23:25:52 +0400

    Exercise - Building a Tree : insert()

--- src/Main.java
+++ src/Main.java
@@ -321,6 +321,17 @@ public class Main {
         System.out.println(hashMap.get(6)); // A+
         hashMap.remove(6);
         System.out.println(hashMap.get(10)); // null
+
+        // *********** Trees ************
+        Tree tree = new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        System.out.println(tree);
     }
 
     public static void log(int[] numbers) {
