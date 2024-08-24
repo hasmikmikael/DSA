c9a462955b8fd243a6a050864cf1b032dcda191f
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-08-25 02:11:58 +0400

    Exercise - Height Calculation

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -1,6 +1,7 @@
 public class AVLTree {
     private class AVLNode {
         private int value;
+        private int height;
         private AVLNode leftChild;
         private AVLNode rightChild;
 
@@ -29,7 +30,14 @@ public class AVLTree {
         else
             root.rightChild = insert(root.rightChild, value);
 
+        root.height = Math.max(
+                height(root.leftChild),
+                height(root.rightChild)) + 1;
+
         return root;
     }
 
+    private int height(AVLNode node) {
+        return (node == null) ? -1 : node.height;
+    }
 }
