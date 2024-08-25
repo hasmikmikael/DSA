0b42dcec6cea50cf233842e25e5872a281d7c758
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-08-26 00:34:01 +0400

    Exercise - Implementing Rotations.

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -30,27 +30,58 @@ public class AVLTree {
         else
             root.rightChild = insert(root.rightChild, value);
 
-        root.height = Math.max(
-                height(root.leftChild),
-                height(root.rightChild)) + 1;
+        setHeight(root);
 
-        balance(root);
+        return balance(root);
+    }
 
+    private AVLNode balance(AVLNode root) {
+        if (isLeftHeavy(root)) {
+            // // System.out.println(root.value + " is left heavy");
+            // if (balanceFactor(root.leftChild) < 0)
+            // System.out.println("Left Rotate " + root.leftChild.value);
+            // System.out.println("Right Rotate " + root.value);
+            if (balanceFactor(root.leftChild) < 0)
+                root.leftChild = rotateLeft(root.leftChild);
+            return rotateRight(root);
+        } else if (isRightHeavy(root)) {
+            // // System.out.println(root.value + " is right heavy");
+            // if (balanceFactor(root.rightChild) > 0)
+            // System.out.println("Right Rotate " + root.rightChild.value);
+            // System.out.println("Left Rotate " + root.value);
+            if (balanceFactor(root.rightChild) > 0)
+                root.rightChild = rotateRight(root.rightChild);
+            return rotateLeft(root);
+        }
         return root;
     }
 
-    private void balance(AVLNode root) {
-        if (isLeftHeavy(root)) {
-            // System.out.println(root.value + " is left heavy");
-            if (balanceFactor(root.leftChild) < 0)
-                System.out.println("Left Rotate " + root.leftChild.value);
-            System.out.println("Right Rotate " + root.value);
-        } else if (isRightHeavy(root)) {
-            // System.out.println(root.value + " is right heavy");
-            if (balanceFactor(root.rightChild) > 0)
-                System.out.println("Right Rotate " + root.rightChild.value);
-            System.out.println("Left Rotate " + root.value);
-        }
+    private AVLNode rotateLeft(AVLNode root) {
+        var newRoot = root.rightChild;
+        root.rightChild = newRoot.leftChild;
+        newRoot.leftChild = root;
+
+        setHeight(root);
+        setHeight(newRoot);
+
+        return newRoot;
+    }
+
+    private AVLNode rotateRight(AVLNode root) {
+        var newRoot = root.leftChild;
+        root.leftChild = newRoot.rightChild;
+        newRoot.rightChild = root;
+
+        setHeight(root);
+        setHeight(newRoot);
+
+        return newRoot;
+    }
+
+    private void setHeight(AVLNode node) {
+        node.height = Math.max(
+                height(node.leftChild),
+                height(node.rightChild)) + 1;
     }
 
     private boolean isLeftHeavy(AVLNode node) {
