f28ddf676b61a33dd6d0c001e185958a65508848
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-08-29 02:12:14 +0400

    Exercise - Heapify : Optimization.

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,6 +1,7 @@
 public class MaxHeap {
     public static void heapify(int[] array) {
-        for (var i = 0; i < array.length; i++)
+        var lastParentIndex = array.length / 2 - 1;
+        for (var i = lastParentIndex; i >= 0; i--)
             heapify(array, i);
     }
 
