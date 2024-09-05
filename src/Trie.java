52f5280a547cf92ce4c37e58c68c072120ff5bb3
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-09-06 02:46:20 +0400

    Exercise - Buiding a Trie : A Better Abstraction.

--- src/Trie.java
+++ src/Trie.java
@@ -17,10 +17,23 @@ public class Trie {
         public String toString() {
             return "value=" + value;
         }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            return children.get(ch);
+        }
     }
 
     private Node root = new Node(' ');
 
+    // // Using an array
     // public void insert(String word) {
     // var current = root;
     // for (var ch : word.toCharArray()) {
@@ -32,12 +45,24 @@ public class Trie {
     // current.isEndOfWord = true;
     // }
 
+    // // Using a HashTable
+    // public void insert(String word) {
+    // var current = root;
+    // for (var ch : word.toCharArray()) {
+    // if (current.children.get(ch) == null)
+    // current.children.put(ch, new Node(ch));
+    // current = current.children.get(ch);
+    // }
+    // current.isEndOfWord = true;
+    // }
+
+    // Using an Abstraction
     public void insert(String word) {
         var current = root;
         for (var ch : word.toCharArray()) {
-            if (current.children.get(ch) == null)
-                current.children.put(ch, new Node(ch));
-            current = current.children.get(ch);
+            if (!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
         }
         current.isEndOfWord = true;
     }
