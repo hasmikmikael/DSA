c0c43d06bee7425a66feabbeb03b664928864aad
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-09-05 23:32:03 +0400

    Exercise - Buiding a Trie : An Implementation with a HashTable.

--- src/Trie.java
+++ src/Trie.java
@@ -1,9 +1,12 @@
+import java.util.HashMap;
+
 public class Trie {
     public static int ALPHABET_SIZE = 26;
 
     private class Node {
         private char value;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        // private Node[] children = new Node[ALPHABET_SIZE];
+        private HashMap<Character, Node> children = new HashMap<>();
         private boolean isEndOfWord;
 
         public Node(char value) {
@@ -18,13 +21,23 @@ public class Trie {
 
     private Node root = new Node(' ');
 
+    // public void insert(String word) {
+    // var current = root;
+    // for (var ch : word.toCharArray()) {
+    // var index = ch - 'a';
+    // if (current.children[index] == null)
+    // current.children[index] = new Node(ch);
+    // current = current.children[index];
+    // }
+    // current.isEndOfWord = true;
+    // }
+
     public void insert(String word) {
         var current = root;
         for (var ch : word.toCharArray()) {
-            var index = ch - 'a';
-            if (current.children[index] == null)
-                current.children[index] = new Node(ch);
-            current = current.children[index];
+            if (current.children.get(ch) == null)
+                current.children.put(ch, new Node(ch));
+            current = current.children.get(ch);
         }
         current.isEndOfWord = true;
     }
