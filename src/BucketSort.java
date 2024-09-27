7183aad7899c4db790b3dc1069e5bfc5744f267b
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-09-28 03:09:13 +0400

    Bucket Sort.

--- src/BucketSort.java
+++ src/BucketSort.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class BucketSort {
-    public void sort(int array[], int numberOfBuckets) {
+    public void sort(int[] array, int numberOfBuckets) {
         int i = 0;
         for (var bucket : createBuckets(array, numberOfBuckets)) {
             Collections.sort(bucket);
